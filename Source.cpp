bf9f1b2fcb11cc57b22283b985a6d6573db131cb
Илья Лысов <Lysovidq@gmail.com>
2021-10-07 19:34:02 +0400

    v3.0

--- Source.cpp
+++ Source.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 string repeat;
 
-double check_input(string flag)
+double check_input(string flag)																//Проверка ввода чисел
 {		
 	if (flag == "double")
 	{
-		double in_value;
+		double in_value;																	//Объявление переменной как double для ввода скорости
 		cin >> in_value;
-		while ((cin.fail()) || (cin.peek() != '\n') || in_value < 0)
+		while ((cin.fail()) || (cin.peek() != '\n') || in_value < 0)						//Проверка не введена ли отрицательная скорость или допущена ошибка при вводе
 		{
-			cin.clear();
-			cin.ignore(99999999, '\n');
+			cin.clear();																	//Очищает флаг ошибки cin
+			cin.ignore(99999999, '\n');														//Очищение буфера на заданное число символов и переходит к следующей новой строке чтобы игнорировать что-либо еще в той же строке
 			cout << "Wrong type of input data, try again" << endl;
 			cout << "Enter the speed in the selected units" << endl;
 			cin >> in_value;
@@ -22,12 +22,12 @@ double check_input(string flag)
 	}
 	else if (flag == "integer")
 	{
-		int in_value;
+		int in_value;																		//Объявление переменной как integer для ввода начальные единиц измерения скорости
 		cin >> in_value;
-		while ((cin.fail()) || (cin.peek() != '\n') || in_value <0)
+		while ((cin.fail()) || (cin.peek() != '\n') || in_value <0)							//Проверка не допущена ли ошибка при вводе
 		{
-			cin.clear();
-			cin.ignore(99999999, '\n');
+			cin.clear();																	//Очищает флаг ошибки cin
+			cin.ignore(99999999, '\n');														//Очищение буфера на заданное число символов и переходит к следующей новой строке чтобы игнорировать что-либо еще в той же строке
 			cout << "Wrong type of input data, try again" << endl;
 			cout << "Select the number of initial speed units" << endl;
 			cout << "1.m/s" << endl;
@@ -38,7 +38,7 @@ double check_input(string flag)
 	}
 }
 
-string repeat_check()
+string repeat_check()																		//Проверка повторения программы
 {
 	cout << "\nRepeat? (Y/N) " << endl;
 	cin >> repeat;
@@ -54,7 +54,7 @@ string repeat_check()
 
 int main()
 {
-	while (repeat != "N" && repeat != "n")
+	while (repeat != "N" && repeat != "n")													//Пока введена не N или n программа работает
 	{
 		cout << "Select the number of initial speed units" << endl;
 		cout << "1.m/s" << endl << "2.km/h" << endl;
@@ -69,18 +69,18 @@ int main()
 		cout << "Enter the speed in the selected units" << endl;
 		speed = check_input("double");
 		double output_value;
-		if (choose == 1)
+		if (choose == 1)																	//Перевод из м/с в км/ч
 		{
 			output_value = speed * 3.6;
 			cout << "Speed in km/h = " << output_value << endl;
 		}
-		if (choose == 2)
+		if (choose == 2)																	//Перевод из км/ч в м/с
 		{
 			output_value = speed / 3.6;
 			cout << "Speed in m/s = " << output_value << endl;
 		}
 
-		repeat = repeat_check();
+		repeat = repeat_check();															//Вызов функции проверки продолжения программы
 	}
 	return 0;
 }
